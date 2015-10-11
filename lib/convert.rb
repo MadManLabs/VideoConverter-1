2c4fbf492e7b178377c3c2e6dab41adff7c5eab5
Mbuckley0 <mitchell.buckley@live.com>
2015-10-11 16:11:04 +1000

    Add ability to convert an entire folder

--- lib/convert.rb
+++ lib/convert.rb
@@ -1,6 +1,17 @@
 require 'streamio-ffmpeg'
 
-def convert(file, to)
+def convert(filepath, from, to)
+	if File.file?(filepath)
+		movie = convert_file(filepath, to) if File.extname(filepath) == from
+	else
+		Dir.	foreach(filepath) do |file|
+			movie = convert_file(filepath + "/" + file, to) if File.extname(file) == from
+		end
+	end
+	return movie
+end
+
+def convert_file(file, to)
 	begin
 		movie = FFMPEG::Movie.new(file)
 		movie.transcode(File.basename(file,File.extname(file)) + to) { |progress| puts progress }
--- lib/ui.rb
+++ lib/ui.rb
@@ -5,27 +5,42 @@ Shoes.app( :width => 500, :height => 500)  do
 	@filename = ''
 	stack margin_left: 150, margin_top: 10 do
 		flow margin_top: 140 do
-			para 'Please select a file'
+			para 'Please select a file or a folder'
 		end
 
 		flow margin_bottom: 50 do
 			@open_file = button 'Open File'
 			@open_file.click {
-				@filename = ask_open_file
+				@filepath = ask_open_file
 				if !@p1
-					@p1 = para File.basename(@filename)
+					@p1 = para File.basename(@filepath)
 				else
-					@p1.text = File.basename(@filename)
+					@p1.text = File.basename(@filepath)
 				end
 			}
 		end
 
+		flow margin_bottom: 50 do
+			@open_folder = button 'Open Folder'
+			@open_folder.click {
+				@filepath = ask_open_folder
+				if !@p1
+					@p1 = para File.basename(@filepath)
+				else
+					@p1.text = File.basename(@filepath)
+				end
+			}
+		end
+
+	para 'Convert From: '
+	@from_format = list_box items: ['.mp4', '.mkv', '.avi', '.mov', '.mpeg', '.ogg', '.flv', '.webm']
+
 	para 'Convert To: '
-	@format = list_box items: ['.mp4', '.mkv', '.avi', '.mov', '.mpeg', '.ogg', '.flv', '.webm']
+	@to_format = list_box items: ['.mp4', '.mkv', '.avi', '.mov', '.mpeg', '.ogg', '.flv', '.webm']
 
 	@convert = button 'Convert Video'
 	@convert.click {
-		@movie = convert(@filename, @format.text)
+		@movie = convert(@filepath, @from_format.text, @to_format.text)
 
 		if(@movie != nil)
 			alert('Conversion Sucessful.')
